66f8b8ea492b9c4aff046fb6642cd0ca1323b2d1
gen-maksim <gen-maksim@mail.ru>
2015-05-28 23:38:18 +0300

    1.4
    Оптимизирован алгоритм пикселей.

--- Brezenhem.py
+++ Brezenhem.py
@@ -3,16 +3,107 @@ import math
 
 
 def pixel(a):  # процедура рисования пикселя
-   # turtle.forward(a)
+    turtle.forward(-a/2)
+    turtle.left(90)
+    turtle.forward(-a/2)
+    turtle.right(90)
     turtle.begin_fill()
     for i in range(4):
         turtle.forward(a)
         turtle.left(90)
     turtle.end_fill()
+    turtle.left(90)
+    turtle.back(-a/2)
+    turtle.right(90)
+    turtle.back(-a/2)
+
+
+def Radius():
+    #radius = math.sqrt(x**2 + y**2)
+    turtle.sety(radius)
+    turtle.setx(0)
+    turtle.up()
+    # дальше начинается сам процесс рисования
+    #выбираем путь, который меньше всего отличается от радиуса
+    while turtle.xcor() < radius and turtle.ycor() > 0:
+        mh1 = int(abs((turtle.xcor() + step)**2 + turtle.ycor() ** 2 - radius**2))
+        md2 = int(abs((turtle.xcor()+step)**2+(turtle.ycor()-step)**2 - radius**2))
+        mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()-step)**2 - radius**2))
+        smallest = 0
+       # print(mh1,md2,mv3)
+        pixel(step)
+        turtle.listen()
+        if mh1 < md2 and mh1 < mv3:
+            turtle.goto((turtle.xcor() + step), turtle.ycor())
+          #  print(1)
+        elif md2 < mh1 and md2 < mv3:
+            turtle.goto((turtle.xcor()+step),turtle.ycor()-step)
+           # print(2)
+        else:
+            turtle.goto((turtle.xcor(),turtle.ycor()-step))
+    pixel(step)
+
+    #4 цикла отвечают за разные дуги окружности
+    
+    while turtle.xcor() > 0 and turtle.ycor() > -radius:
+        mh1 = int(abs((turtle.xcor() - step)**2 + turtle.ycor() ** 2 - radius**2))
+        md2 = int(abs((turtle.xcor()- step)**2+(turtle.ycor()-step)**2 - radius**2))
+        mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()-step)**2 - radius**2))
+        smallest = 0
+       # print(mh1,md2,mv3)
+        pixel(step)
+        turtle.listen()
+        if mh1 < md2 and mh1 < mv3:
+            turtle.goto((turtle.xcor() - step), turtle.ycor())
+          #  print(1)
+        elif md2 < mh1 and md2 < mv3:
+            turtle.goto((turtle.xcor()-step),turtle.ycor()-step)
+           # print(2)
+        else:
+            turtle.goto((turtle.xcor(),turtle.ycor()-step))
+    pixel(step)
+            
+    while turtle.xcor() >= -radius and turtle.ycor() <0:
+        mh1 = int(abs((turtle.xcor() - step)**2 + turtle.ycor() ** 2 - radius**2))
+        md2 = int(abs((turtle.xcor()-step)**2+(turtle.ycor()+step)**2 - radius**2))
+        mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()+step)**2 - radius**2))
+        smallest = 0
+       # print(mh1,md2,mv3)
+        pixel(step)
+        turtle.listen()
+        if mh1 < md2 and mh1 < mv3:
+            turtle.goto((turtle.xcor() - step), turtle.ycor())
+          #  print(1)
+        elif md2 < mh1 and md2 < mv3:
+            turtle.goto((turtle.xcor()-step),turtle.ycor()+step)
+           # print(2)
+        else:
+            turtle.goto((turtle.xcor(),turtle.ycor()+step))
+    pixel(step)      
+    while turtle.xcor() < 0:
+        mh1 = int(abs((turtle.xcor() + step)**2 + turtle.ycor() ** 2 - radius**2))
+        md2 = int(abs((turtle.xcor()+step)**2+(turtle.ycor()+step)**2 - radius**2))
+        mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()+step)**2 - radius**2))
+        smallest = 0
+       # print(mh1,md2,mv3)
+        pixel(step)
+        turtle.listen()
+        if mh1 < md2 and mh1 < mv3:
+            turtle.goto((turtle.xcor() + step), turtle.ycor())
+          #  print(1)
+        elif md2 < mh1 and md2 < mv3:
+            turtle.goto((turtle.xcor()+step),turtle.ycor()+step)
+           # print(2)
+        else:
+            turtle.goto((turtle.xcor(),turtle.ycor()+step))
+    pixel(step)
+    
+
+
 
 turtle.listen()
 turtle.bye()  # обновляем поле
-turtle.ht()  # скрываем черпаху
+#turtle.ht()  # скрываем черпаху
 turtle.speed(0.5)  # рисуем сетку. увеличиваем скорость, для
 for i in range(4):  # большей продуктивности
     turtle.left(90 * i)
@@ -31,104 +122,14 @@ turtle.down()
 
 turtle.speed(0.1)  # для наглядности уменьшим скорость
 
-"""if x1 >= y1:  # рассматриваем два варианта:
-    axis = 1  # потому что алгоритмы отличаются
-    kf = y1 / x1
-else:
-    kf = kf = x1 / y1
-    axis = 0"""
 
-#print("Угловой коэффициент: ", kf)
+
 check = 0
 grad = 90
-radius = 80
 step = 4
-"""if axis == 0:
-    turtle.forward(a)
-    turtle.left(90)
-    grad = -90"""
-turtle.sety(radius)
-turtle.setx(0)
-# дальше начинается сам процесс рисования
-turtle.down()
-while turtle.xcor() < radius and turtle.ycor() > 0:
-    mh1 = int(abs((turtle.xcor() + step)**2 + turtle.ycor() ** 2 - radius**2))
-    md2 = int(abs((turtle.xcor()+step)**2+(turtle.ycor()-step)**2 - radius**2))
-    mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()-step)**2 - radius**2))
-    smallest = 0
-   # print(mh1,md2,mv3)
-    pixel(step)
-    turtle.listen()
-    if mh1 < md2 and mh1 < mv3:
-        turtle.goto((turtle.xcor() + step), turtle.ycor())
-      #  print(1)
-    elif md2 < mh1 and md2 < mv3:
-        turtle.goto((turtle.xcor()+step),turtle.ycor()-step)
-       # print(2)
-    else:
-        turtle.goto((turtle.xcor(),turtle.ycor()-step))
-pixel(step)
 
-while turtle.xcor() > 0 and turtle.ycor() > -radius:
-    mh1 = int(abs((turtle.xcor() - step)**2 + turtle.ycor() ** 2 - radius**2))
-    md2 = int(abs((turtle.xcor()-step)**2+(turtle.ycor()-step)**2 - radius**2))
-    mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()-step)**2 - radius**2))
-    smallest = 0
-   # print(mh1,md2,mv3)
-    pixel(step)
-    turtle.listen()
-    if mh1 < md2 and mh1 < mv3:
-        turtle.goto((turtle.xcor() - step), turtle.ycor())
-      #  print(1)
-    elif md2 < mh1 and md2 < mv3:
-        turtle.goto((turtle.xcor()-step),turtle.ycor()-step)
-       # print(2)
-    else:
-        turtle.goto((turtle.xcor(),turtle.ycor()-step))
-pixel(step)
-        
-while turtle.xcor() >= -radius and turtle.ycor() <0:
-    mh1 = int(abs((turtle.xcor() - step)**2 + turtle.ycor() ** 2 - radius**2))
-    md2 = int(abs((turtle.xcor()-step)**2+(turtle.ycor()+step)**2 - radius**2))
-    mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()+step)**2 - radius**2))
-    smallest = 0
-   # print(mh1,md2,mv3)
-    pixel(step)
-    turtle.listen()
-    if mh1 < md2 and mh1 < mv3:
-        turtle.goto((turtle.xcor() - step), turtle.ycor())
-      #  print(1)
-    elif md2 < mh1 and md2 < mv3:
-        turtle.goto((turtle.xcor()-step),turtle.ycor()+step)
-       # print(2)
-    else:
-        turtle.goto((turtle.xcor(),turtle.ycor()+step))
-pixel(step)      
-while turtle.xcor() < 0:
-    mh1 = int(abs((turtle.xcor() + step)**2 + turtle.ycor() ** 2 - radius**2))
-    md2 = int(abs((turtle.xcor()+step)**2+(turtle.ycor()+step)**2 - radius**2))
-    mv3 = int(abs(turtle.xcor()**2+(turtle.ycor()+step)**2 - radius**2))
-    smallest = 0
-   # print(mh1,md2,mv3)
-    pixel(step)
-    turtle.listen()
-    if mh1 < md2 and mh1 < mv3:
-        turtle.goto((turtle.xcor() + step), turtle.ycor())
-      #  print(1)
-    elif md2 < mh1 and md2 < mv3:
-        turtle.goto((turtle.xcor()+step),turtle.ycor()+step)
-       # print(2)
-    else:
-        turtle.goto((turtle.xcor(),turtle.ycor()+step))
-pixel(step)
-""" turtle.backward(a)
-        check -= 1
-        turtle.up()
-        turtle.left(grad)
-        turtle.forward(a)
-        turtle.right(grad)
-        turtle.forward(a)
-        turtle.down()"""
+radius = turtle.numinput("Радиус", "Введите радиус круга:", 300, minval=10, maxval=300)
+Radius()
 
 
 
--- Client.py
+++ Client.py
@@ -1,7 +1,7 @@
-from Brezenhem import line
-x1 = int(input('введите х: '))  # принимаем данные
-y1 = int(input('введите у: '))
-a = int(input('Задайте размер пикселя: '))
-line(x1, y1, a)
-input('намите Enter, чтобы закончить')
+#from Brezenhem import line
+#x1 = int(input('введите х: '))  # принимаем данные
+#y1 = int(input('введите у: '))
+#a = int(input('Задайте размер пикселя: '))
+#line(x1, y1, a)
+input('Клиентская часть не работает, запускайте модуль')
 exit()
